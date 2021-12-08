9ce142a033ddca39afb6dbeb07ddbfda890b54cb
rkonesky <rosskon1023@gmail.com>
2021-12-07 22:20:22 -0600

    added framework to server.js and added public folder structure

---
+++ server.js
@@ -0,0 +1,41 @@
+// Require Dependencies 
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+require('dotenv').config();
+/* TO DO: ADD CONTROLLERS */
+
+
+
+// Initialize the Application
+const app = express();
+
+
+// Configure Application Settings
+const {DATABASE_URL, PORT, SECRET, API_KEY} = process.env;
+
+
+// Connect to and Configure Database
+mongoose.connect(DATABASE_URL);
+
+
+// Set up Database Listeners
+const db = mongoose.connection;
+db.on("error", (err) => console.log("Mongo is not running. Error"));
+db.on("connected", () => console.log("Mongo Connected"));
+db.on("disconnected", () => console.log("Mongo is disconnected"));
+
+// Mount Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+
+// Mount Routes
+/* TO DO: Mount Routes
+app.use('/', xxxxController);
+*/
+
+
+// Check if Application is Listening
+app.listen(PORT, () => console.log (`Server is listening on port: ${PORT}`));
